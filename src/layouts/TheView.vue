5dbf99882acb13258441f1414ace84c73a1e18e4
anko-developer <59523270+anko-developer@users.noreply.github.com>
2023-06-23 06:20:26 +0900

    settings

---
+++ src/layouts/TheView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <div class="container mt-2 pt-5">
+      <RouterView></RouterView>
+    </div>
+  </main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
